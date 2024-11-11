42c4f9f8f48618c0e0df61be70d8c6c43c11028b
Adarsh <aadarshg.krishnan@gmail.com>
2024-11-11 23:22:37 +0530

    code update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import logo from "../assets/AKLogo.png";
 import {FaLinkedin} from "react-icons/fa"
 import {FaGithub} from "react-icons/fa"
 import {FaEnvelope} from "react-icons/fa"
@@ -6,7 +7,7 @@ import {FaPhone} from "react-icons/fa"
 const Navbar = () => {
   return <nav className="mb-20 flex items-center justify-between py-6">
     <div>
-        <img className="mx-2 w-10" src="/AKLogo.png" alt="logo"/>
+        <img className="mx-2 w-10" src={logo} alt="logo"/>
     </div>
     <div className="m-8 flex item-center justify-center gap-4 text-2xl">
         <FaLinkedin/>
